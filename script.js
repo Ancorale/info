8c1924852cb1d31a4bf805e43571afb8f51fcf07
Ancorale <ancorale@ancorale.com>
2018-10-24 11:47:07 -0700

    10.24.18..1146

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
   `use strict`;
 
+  // DATE FORMATER
+  function convertDate() {
+    var d = new Date();
+    /*  var a = [(d.getMonth() + 1),
+        (d.getDate()),
+        (d.getFullYear()),
+      ].join('/');  */
+    var b = [(d.getHours()),
+      (d.getMinutes()),
+      (d.getSeconds()),
+    ].join(':');
+    return (b + ' ');
+  }
+  console.log(convertDate());
+
+
+  // Time
+  $(`#time`).text(convertDate());
+
+  // button push
   $(`.num`).on(`click`, function() {
     $(this).addClass(`push`);
-    setTimeout(function(){
+    setTimeout(function() {
       $(`.num`).removeClass(`push`);
     }, 200);
 
