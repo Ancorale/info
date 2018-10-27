f2f2c4db6997bb0b04b63931a4a3f4dbcca20943
Ancorale <ancorale@ancorale.com>
2018-10-26 22:00:02 -0700

    10.26.18..2158

--- script.js
+++ script.js
@@ -40,34 +40,30 @@ $(document).ready(function() {
   // Time
   $(`#time`).text(convertDate());
 
-  // textarea
-  // $(`textarea`).on('change focus', function() {
-  //   var txArea = $(this).val() + " &#64; ";
-  //   var itemTime = convertDate() + " &#13; ";
-  //   var itemName = $(this).prop('id');
-  //   var item = {};
-  //   item[itemName] = txArea;
-  //   item["TIME"] = itemTime;
-  //   myItem.push(item);
-  //   localStorage.setItem('report', JSON.stringify(myItem));
-  //   console.log(myItem);
-  // });
 
   // button push
   $(`.num`).on(`click`, function() {
-    $(`textarea`).on('focus', function() {
-      if (numKey.length <= 3) {
-        console.log(numKey.length);
-        $(this).val(numKey);// set number in box
-      }
-    })
 
-    $(this).addClass(`push`);
+      $(this).addClass(`push`);
       setTimeout(function() {
         $(`.num`).removeClass(`push`);
       }, 100);
-    numKey += $(this).html();// gets button number
 
-  });// end button push
+      numKey += $(this).html(); // gets button number
 
-});// end doc ready
+      let textareaId = $(`textarea`).attr(`id`);
+
+      for (i = 0; i < boxes.length; i++) {
+        if (textareaId === i && numKey.length <= 3) {
+          $(`#` + i).val(numKey); // set number in box
+        }
+        else {
+          var j = i + 1;
+        }
+        console.log(textareaId, numKey.length, i);
+      }
+
+  }); // end button push
+
+
+}); // end doc ready
