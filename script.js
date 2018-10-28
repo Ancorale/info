71a9556184891655c58ee0e63a612ccdcb859d69
Ancorale <ancorale@ancorale.com>
2018-10-27 21:09:07 -0700

    10.27.18..2109

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ $(document).ready(function() {
 
 
   const boxes = {
-    systolic,
-    diastolic,
-    pulse,
-    respiration,
-    SPO2,
-    CO2,
-    bloodSugar,
-    pain,
-    temperature
+    1: `systolic`,
+    2: `diastolic`,
+    3: `pulse`,
+    4: `respiration`,
+    5: `SPO2`,
+    6: `CO2`,
+    7: `bloodSugar`,
+    8: `pain`,
+    9: `temperature`
   };
-  let numKey = ``;
+
 
   // resizer
-  $(window).on('resize', function() {
-    let win = $(this);
-    if (win.width() < 800) {
-      $(`.one1123`).addClass(`marbot20`);
-    } else {
-      $(`.one1123`).removeClass(`marbot20`);
-    }
-  });
+  // $(window).on('resize', function() {
+  //   let win = $(this);
+  //   if (win.width() < 800) {
+  //     $(`.one1123`).addClass(`marbot20`);
+  //   } else {
+  //     $(`.one1123`).removeClass(`marbot20`);
+  //   }
+  // });
 
   // DATE FORMATER
   function convertDate() {
@@ -40,30 +40,40 @@ $(document).ready(function() {
   // Time
   $(`#time`).text(convertDate());
 
-
+  let numKey = ``;
   // button push
   $(`.num`).on(`click`, function() {
+    let textareaId = $(`textarea`).attr(`id`);
 
-      $(this).addClass(`push`);
-      setTimeout(function() {
-        $(`.num`).removeClass(`push`);
-      }, 100);
+    $(this).addClass(`push`);
+    setTimeout(function() {
+      $(`.num`).removeClass(`push`);
+    }, 100);
 
-      numKey += $(this).html(); // gets button number
+    numKey += $(this).html(); // gets button number
+    console.log(typeof(numKey));
 
-      let textareaId = $(`textarea`).attr(`id`);
+    let c = 1;
 
-      for (i = 0; i < boxes.length; i++) {
-        if (textareaId === i && numKey.length <= 3) {
-          $(`#` + i).val(numKey); // set number in box
-        }
-        else {
-          var j = i + 1;
-        }
-        console.log(textareaId, numKey.length, i);
+    for (i in boxes){
+
+      if (numKey.length <= 3) {
+        console.log(c);
+        $(`#` + boxes[c]).val(numKey); // set number in box
+
+      }
+      else {
+        numKey = ``;
+        c++;
+
+        $(`#` + boxes[c]).val(numKey);
+          console.log(boxes[c]);
+
       }
 
-  }); // end button push
+    }// end for i
 
+
+  }); // end button push
 
 }); // end doc ready
